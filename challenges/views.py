3799696f59bbc70934a047a057f95a3431b09017
Karthik-Billava <karthikbillava1107@gmail.com>
2025-02-19 16:23:15 +0530

    23. Adding More Views & URLs

--- challenges/views.py
+++ challenges/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("It works!")
+def january(request):
+    return HttpResponse("Exercise for 30 minutes daily")
+
+def february(request):
+    return HttpResponse("No junk food")

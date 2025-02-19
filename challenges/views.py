2b6f3993aab6171b6df678e4a599a898b7a9eb99
Karthik-Billava <karthikbillava1107@gmail.com>
2025-02-19 16:41:04 +0530

    24. Dynamic Path Segments & Captured Values

--- challenges/views.py
+++ challenges/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
-def january(request):
-    return HttpResponse("Exercise for 30 minutes daily")
 
-def february(request):
-    return HttpResponse("No junk food")
+def monthly_challenges(request, month):
+    challenge_text=None
+    if month == "january":
+        challenge_text="Exercise for 30 minutes daily"
+    elif month=="february":
+        challenge_text="No junk food"
+    elif month=="march":
+        challenge_text="Meditate for 30 minutes"
+    else:
+        return HttpResponseNotFound("This month is not supported yet!")
+    return HttpResponse(challenge_text)

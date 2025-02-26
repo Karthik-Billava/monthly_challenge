1e736f12bef67b4be29b0c80fb4594ca74d0a4ca
Karthik-Billava <karthikbillava1107@gmail.com>
2025-02-26 12:09:59 +0530

    25. Path Converters

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+
+def monthly_challenges_int(request, month):
+    return HttpResponse(month)
+
+
 def monthly_challenges(request, month):
-    challenge_text=None
+    challenge_text = None
     if month == "january":
-        challenge_text="Exercise for 30 minutes daily"
-    elif month=="february":
-        challenge_text="No junk food"
-    elif month=="march":
-        challenge_text="Meditate for 30 minutes"
+        challenge_text = "Exercise for 30 minutes daily"
+    elif month == "february":
+        challenge_text = "No junk food"
+    elif month == "march":
+        challenge_text = "Meditate for 30 minutes"
     else:
         return HttpResponseNotFound("This month is not supported yet!")
     return HttpResponse(challenge_text)
